14b69c6fccf0a005c3ec0a64f2f69fcc96b8f15b
Yonggang Shi <yonggangs@opera.com>
2015-07-28 10:34:36 +0800

    fix bug in push script that gitcafe is not updated when param is all.

--- push_source.sh
+++ push_source.sh
@@ -3,9 +3,10 @@
 if [ "$1" == "github" ] || [ "$1" == "all" ]
 then
     git push github HEAD:source
-elif [ "$1" == "gitcafe" ] || [ "$1" == "all" ]
+fi
+
+if [ "$1" == "gitcafe" ] || [ "$1" == "all" ]
 then
     git push gitcafe HEAD:source
-else
-    echo "dest wrong"
 fi
+
